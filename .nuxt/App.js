89b5a75142d235886bc2a4620ec4ca70d1a04008
yun.ma <mayun@qizhidao.com>
2020-01-17 19:13:46 +0800

    sss

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '..\\node_modules\\_element-ui@2.12.0@element-ui\\lib\\theme-chalk\\index.css'
 
@@ -45,7 +44,7 @@ export default {
       domProps: {
         id: '__nuxt'
       }
-    }, [loadingEl, h(NuxtBuildIndicator), transitionEl])
+    }, [loadingEl, transitionEl])
   },
   data: () => ({
     isOnline: true,
@@ -104,8 +103,6 @@ export default {
     },
 
     setLayout(layout) {
-      if(layout && typeof layout !== 'string') throw new Error('[nuxt] Avoid using non-string value as layout property.')
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
